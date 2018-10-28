b7507ad3a57d8832106f43d47f47a598afa10516
jinfuchao <317519656@qq.com>
2018-10-28 16:17:37 +0800

    把本地工程添加到git

---
+++ git_first_project/src/com/itheima/FirstGit.java
@@ -0,0 +1,14 @@
+package com.itheima;
+
+public class FirstGit {
+    public static void main(String[] args) {
+        System.out.println("你好Git");
+        System.out.println("你好Git");
+        System.out.println("你好Git");
+        System.out.println("你好Git");
+        System.out.println("你好Git");
+        System.out.println("你好Git");
+        System.out.println("你好Git");
+        System.out.println("你好Git");
+    }
+}
