d0bfa4caaf1dec88b779c1d121f1b6cbc5a55466
jinfuchao <317519656@qq.com>
2018-10-28 16:57:00 +0800

    修改FirstGit类

--- git_first_project/src/com/itheima/FirstGit.java
+++ git_first_project/src/com/itheima/FirstGit.java
@@ -10,10 +10,12 @@ public class FirstGit {
         System.out.println("你好Git");
         System.out.println("你好Git");
         System.out.println("你好Git");
+        //修改test
     }
     public void show(){
         int i =1;
         i=i+1;
         System.out.println(i);
+        System.out.println("修改过的");
     }
 }
